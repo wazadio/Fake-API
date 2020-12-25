6aa378c8b7763361dcc4a214e34686e4d75e17ab
wazadio <junidio306@gmail.com>
2020-12-25 18:44:17 +0700

    ok

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000;
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -27,6 +28,6 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
